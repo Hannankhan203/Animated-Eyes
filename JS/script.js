733f1421d7303a806e5b06ad2961e8979b98af44
Hannankhan203 <ak1818375@gmail.com>
2024-11-10 14:01:40 +0500

    Added blinking eyeball effect

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,24 @@
 const body = document.querySelector("body");
 const eyes = document.querySelectorAll(".eye");
 
-
 const eyeball = (event) => {
     eyes.forEach((eye) => {
         let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
         let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
         let radian = Math.atan2(event.pageX - x, event.pageY - y);
         let rotation = (radian * (180 / Math.PI) * -1) + 270;
-        eye.style.transform = "rotate("+rotation+"deg)"
+        eye.style.transform = "rotate(" + rotation + "deg)";
     });
-}
+};
 
-body.addEventListener("mousemove", eyeball)
+body.addEventListener("mousemove", eyeball);
+
+eyes.forEach((eye) => {
+    eye.addEventListener("mousedown", () => {
+        eye.classList.add("close");
+    });
+
+    eye.addEventListener("mouseup", () => {
+        eye.classList.remove("close");
+    });
+});
